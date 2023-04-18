7cd5dc5fbce1f933005bc987155c483036ba1ce0
Ping Yang <ping.yang@sas.com>
2023-04-18 09:48:40 -0400

    Completed module

--- budget/Expense.py
+++ budget/Expense.py
@@ -20,6 +20,21 @@ class Expenses():
                 amount = float((row[3][2:]).replace(',',''))
                 self.list.append(Expense(row[0],row[1], row[2], amount))
                 self.sum += amount
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+
     def categorize_for_loop(self):
             necessary_expenses2 = set()
             food_expenses2 = set()
@@ -33,7 +48,9 @@ class Expenses():
                     food_expenses2.add(i)
                 else:
                     unnecessary_expenses2.add(i)
-        return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
+
+            return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
+            
 
     def categorize_set_comprehension(self):
         necessary_expenses = {x for x in self.list 
@@ -46,4 +63,4 @@ class Expenses():
 
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
 
-        return [necessary_expenses, food_expenses, unnecessary_expenses]                
+        return [necessary_expenses, food_expenses, unnecessary_expenses]       
